369fd34f480fda6fa9af99540eb44a5af649a855
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-02 16:04:22 +0400

    Implementing a Service

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,1 +1,21 @@
-export class MessagesService {}
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
